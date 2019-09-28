db8afe2caa47a8c978c5860ce9d84d22f82f2761
canyener <can.yener@hotmail.com.tr>
2019-09-29 02:34:54 +0300

    Removes save method from signup endpoint while it already exists in generateAuthToken instance method

--- src/routers/user.js
+++ src/routers/user.js
@@ -8,7 +8,6 @@ router.post('/users', async (req, res) => {
     const user = new User(req.body)
 
     try {
-        await user.save()
         const token = await user.generateAuthToken()
         res.status(201).send({ user, token })
     } catch (e) {
