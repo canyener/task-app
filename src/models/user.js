17c42d5bba19ec607e4126b6bbaa23499e8025ba
canadaslar <can@adaslar.co.uk>
2019-09-28 21:40:41 +0300

    Adds unique property to email

--- src/models/user.js
+++ src/models/user.js
@@ -11,6 +11,7 @@ const userSchema = new mongoose.Schema({
     email: {
         type: String,
         required: true,
+        unique: true,
         trim: true,
         lowercase: true,
         validate(value) {
