0b298ef22a9f84372b496938f2f58aa767af1a4d
canyener <can.yener@hotmail.com.tr>
2019-09-30 00:10:27 +0300

    Adds authentication to PATCH /tasks/:id

--- src/routers/task.js
+++ src/routers/task.js
@@ -50,7 +50,7 @@ router.get('/tasks/:id', auth, async (req, res) => {
     }
 })
 
-router.patch('/tasks/:id', async (req, res) => {
+router.patch('/tasks/:id', auth, async (req, res) => {
     const updates = Object.keys(req.body)
     const allowedUpdates = ['description', 'completed']
     const isValidOperation = updates.every(update => allowedUpdates.includes(update))
@@ -60,16 +60,14 @@ router.patch('/tasks/:id', async (req, res) => {
     }
 
     try {
-
-        const task = await Task.findById(req.params.id)
-        updates.forEach(update => task[update] = req.body[update])
-
-        task.save()
+        const task = await Task.findOne({ _id: req.params.id, owner: req.user._id })
 
         if(!task) {
             return res.status(404).send()
         }
 
+        updates.forEach(update => task[update] = req.body[update])
+        task.save()
         res.send(task)
     } catch (e) {
          //if validation error occurs
