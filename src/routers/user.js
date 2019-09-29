84b86b50c537492d68eb8adc59a8b505028ab33c
canyener <can.yener@hotmail.com.tr>
2019-09-29 21:46:25 +0300

    Adds logout all endpoint

--- src/routers/user.js
+++ src/routers/user.js
@@ -40,6 +40,17 @@ router.post('/users/logout', auth, async (req, res) => {
     }
 })
 
+router.post('/users/logoutAll', auth, async (req, res) => {
+    try {
+        req.user.tokens = []
+        await req.user.save()
+
+        res.send()
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
 router.get('/users/me', auth, async (req, res) => {
    res.send(req.user)
 })
