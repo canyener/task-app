60872c7285bbfc65b9912a4b6941a3fe9171cb87
canyener <can.yener@hotmail.com.tr>
2019-09-30 00:19:54 +0300

    Adds authentication to DELETE /tasks/:id

--- src/routers/task.js
+++ src/routers/task.js
@@ -75,9 +75,10 @@ router.patch('/tasks/:id', auth, async (req, res) => {
     }
 })
 
-router.delete('/tasks/:id', async (req, res) => {
+router.delete('/tasks/:id', auth, async (req, res) => {
     try {
-        const task = await Task.findByIdAndDelete(req.params.id)
+        const task = await Task.findOneAndDelete({ _id: req.params.id, owner: req.user._id })
+
         if(!task) {
             return res.status(404).send()
         }
