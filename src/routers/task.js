3b9da0a8bcca547e42eb5f488ed3644fc4ca198c
canyener <can.yener@hotmail.com.tr>
2019-09-29 22:50:07 +0300

    Refactors POST /tasks endpoint

--- src/routers/task.js
+++ src/routers/task.js
@@ -6,7 +6,11 @@ const auth = require('../middleware/auth')
 const router = new express.Router()
 
 router.post('/tasks', auth, async (req, res) => {
-    const task = new Task(req.body)
+    
+    const task = new Task({
+        ...req.body,
+        owner: req.user._id
+    }) 
 
     try {
         await task.save()
