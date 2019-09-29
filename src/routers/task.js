4e991726c15f909614c9c9befcd4d4521c1a94c6
canyener <can.yener@hotmail.com.tr>
2019-09-29 23:43:46 +0300

    Refactors GET /tasks/id endpoint query and makes it get tasks only owned by authenticated user

--- src/routers/task.js
+++ src/routers/task.js
@@ -33,7 +33,8 @@ router.get('/tasks/:id', auth, async (req, res) => {
     const _id = req.params.id
 
     try {
-        const task = await Task.findById(_id)
+        const task = await Task.findOne({ _id, owner: req.user._id })
+        
         if(!task) {
             return res.status(404).send()
         }
