a6b88e3d8b2bef68c9e5da3213c966658677f402
canyener <can.yener@hotmail.com.tr>
2019-09-29 21:22:46 +0300

    Fixes issue #4

--- src/routers/user.js
+++ src/routers/user.js
@@ -9,6 +9,7 @@ router.post('/users', async (req, res) => {
     const user = new User(req.body)
 
     try {
+        await user.save()
         const token = await user.generateAuthToken()
         res.status(201).send({ user, token })
     } catch (e) {
