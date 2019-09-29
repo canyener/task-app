62fd4a8ad2867e7b98f6f9be085c29ed215cd26d
canyener <can.yener@hotmail.com.tr>
2019-09-29 21:36:26 +0300

    Adds logic to logout endpoint

--- src/routers/user.js
+++ src/routers/user.js
@@ -28,7 +28,16 @@ router.post('/users/login', async (req, res) => {
 })
 
 router.post('/users/logout', auth, async (req, res) => {
-    
+    try {
+        req.user.tokens = req.user.tokens.filter(token => {
+            return token.token !== req.token
+        })
+        await req.user.save()
+
+        res.send()
+    } catch (e) {
+        res.status(500).send()
+    }
 })
 
 router.get('/users/me', auth, async (req, res) => {
