4db32d232f91b2038b356ca09a44b582c99e06d8
canyener <can.yener@hotmail.com.tr>
2019-09-29 22:39:20 +0300

    Adds owner property to Task Schema

--- src/models/task.js
+++ src/models/task.js
@@ -9,6 +9,10 @@ const taskSchema = new mongoose.Schema({
     completed: {
         type: Boolean,
         default: false
+    },
+    owner : {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true
     }
 })
 
