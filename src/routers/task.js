8c6e918892890fa38cda637cc883cb970863b3a5
canyener <can.yener@hotmail.com.tr>
2019-09-29 22:40:28 +0300

    Adds auth middleware and integrates it to POST /tasks

--- src/routers/task.js
+++ src/routers/task.js
@@ -1,10 +1,11 @@
 const express = require('express')
 
 const Task = require('../models/task')
+const auth = require('../middleware/auth')
 
 const router = new express.Router()
 
-router.post('/tasks', async (req, res) => {
+router.post('/tasks', auth, async (req, res) => {
     const task = new Task(req.body)
 
     try {
