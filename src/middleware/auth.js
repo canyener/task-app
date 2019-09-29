8dfabb762d0ed03c5f3db5ccfc741e2efbba2cb1
canyener <can.yener@hotmail.com.tr>
2019-09-29 19:39:16 +0300

    Adds middleware to GET /users and adds required modules for authentication middleware

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,6 +1,8 @@
+const jwt = require('jsonwebtoken')
+const User = require('../models/user')
+
 const auth = async (req, res, next) => {
-    console.log('auth middleware!')
-    next()
+    
 }
 
 module.exports = auth
--- src/routers/user.js
+++ src/routers/user.js
@@ -1,6 +1,7 @@
 const express = require('express')
 
 const User = require('../models/user')
+const auth = require('../middleware/auth')
 
 const router = new express.Router()
 
@@ -25,7 +26,7 @@ router.post('/users/login', async (req, res) => {
     }
 })
 
-router.get('/users', async (req, res) => {
+router.get('/users', auth, async (req, res) => {
     try {
         const users = await User.find({})
         res.send(users)
