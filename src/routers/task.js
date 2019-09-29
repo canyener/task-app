28025a7dc6b7471920fe108fab2063b1bd7d1cb5
canyener <can.yener@hotmail.com.tr>
2019-09-30 00:03:37 +0300

    Adds authentication to GET /tasks endpoint

--- src/routers/task.js
+++ src/routers/task.js
@@ -20,10 +20,14 @@ router.post('/tasks', auth, async (req, res) => {
     }
 })
 
-router.get('/tasks', async (req, res) => {
+router.get('/tasks', auth, async (req, res) => {
     try {
-        const tasks = await Task.find({})
+        const tasks = await Task.find({ owner: req.user._id})
         res.send(tasks)
+
+        //Alternative way
+        // await req.user.populate('tasks').execPopulate()
+        // res.send(req.user.tasks)
     } catch (e) {
         res.status(500).send()
     }
