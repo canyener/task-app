6d8aad50dde7668a3314c55c66677383d27ae0b0
canyener <can.yener@hotmail.com.tr>
2019-10-02 20:08:24 +0300

    Adds fileFilter method to create and example upload which only accepts Word documents under 1Mb file size

--- playground/file-upload.js
+++ playground/file-upload.js
@@ -7,6 +7,13 @@ const upload = multer({
     dest: 'images',
     limits: {
         fileSize: 1000000 //1 MB
+    },
+    fileFilter(req, file, cb) {
+        if (!file.originalname.match(/\.(doc|docx)$/)) {
+            return cb(new Error('Please upload a Word document'))
+        }
+
+        cb(undefined, true)
     }
 })
 
