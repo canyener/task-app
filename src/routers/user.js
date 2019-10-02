e87e34ec266ebd381a5e0bdfd49ca062be2f47dc
canyener <can.yener@hotmail.com.tr>
2019-10-02 22:00:29 +0300

    Passes a second function to POST /users/me/avatar to handle errors

--- src/routers/user.js
+++ src/routers/user.js
@@ -8,7 +8,7 @@ const router = new express.Router()
 const upload = multer({
     dest: 'avatars',
     limits: {
-        fileSize: 1000000 // 1 MB
+        fileSize: 1000000 // 1 MB        
     },
     fileFilter(req, file, cb) {
 
@@ -103,6 +103,8 @@ router.delete('/users/me', auth, async (req, res) => {
 
 router.post('/users/me/avatar', upload.single('avatar'), async (req, res) => {
     res.send()
+}, (error, req, res, next) => {
+    res.status(400).send({error: error.message})
 })
 
 module.exports = router
