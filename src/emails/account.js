dec68ad6a9ad5b1501f1eedbdf8188a6d280a8e6
canyener <can.yener@hotmail.com.tr>
2019-10-05 20:21:21 +0300

    Adds example code to send email

--- src/emails/account.js
+++ src/emails/account.js
@@ -1,1 +1,12 @@
+const sgMail = require('@sendgrid/mail')
+
 const sendgridAPIKey = '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'
+
+sgMail.setApiKey(sendgridAPIKey)
+
+sgMail.send({
+    to:'can.yener@hotmail.com.tr',
+    from: 'duneverjudgeme@gmail.com',
+    subject: 'Mail from nodejs',
+    text: 'Sent from node task app'
+})
