9c2f3b8f78be793d928d5cacdd91153d7f08d96c
canyener <can.yener@hotmail.com.tr>
2019-10-06 18:59:53 +0300

    Adds test suites for temperature functions

--- playground/math.js
+++ playground/math.js
@@ -1,12 +1,8 @@
 const calculateTip = (total, tipPercent = .25) => total + (total * tipPercent)
 
-const fahrenheitToCelcius = (temp) => {
-    return (temp - 32) / 1.8
-}
+const fahrenheitToCelcius = (temp) => (temp - 32) / 1.8
 
-const celciusToFahrenheit = (temp) => {
-    return (temp * 1.8) + 32
-}
+const celciusToFahrenheit = (temp) => (temp * 1.8) + 32
 
 module.exports = {
     calculateTip,
--- playground/math.test.js
+++ playground/math.test.js
@@ -1,4 +1,4 @@
-const {calculateTip} = require('./math')
+const {calculateTip, fahrenheitToCelcius, celciusToFahrenheit} = require('./math')
 
 test('Should calculate total with tip', () => {
     const expected = 13
@@ -9,5 +9,17 @@ test('Should calculate total with tip', () => {
 test('Should calculate total with default tip, if tip not provided', () => {
     const expected = 12.5
     const actual = calculateTip(10)
+    expect(actual).toBe(expected)
+})
+
+test('Should convert 32 F to 0 C', () => {
+    const expected = 0
+    const actual = fahrenheitToCelcius(32)
+    expect(actual).toBe(expected)
+})
+
+test('Should convert 0 C to 32 F', () => {
+    const expected = 32
+    const actual = celciusToFahrenheit(0)
     expect(actual).toBe(expected)
 })
