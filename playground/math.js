f038bcfba2e018787ab9b2022a6bc42c2b6b9f53
canyener <can.yener@hotmail.com.tr>
2019-10-06 18:51:51 +0300

    Adds temperature converting functions

--- playground/math.js
+++ playground/math.js
@@ -1,5 +1,15 @@
 const calculateTip = (total, tipPercent = .25) => total + (total * tipPercent)
 
+const fahrenheitToCelcius = (temp) => {
+    return (temp - 32) / 1.8
+}
+
+const celciusToFahrenheit = (temp) => {
+    return (temp * 1.8) + 32
+}
+
 module.exports = {
-    calculateTip
+    calculateTip,
+    fahrenheitToCelcius,
+    celciusToFahrenheit
 }
