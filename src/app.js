ccb21f05d4926c36d0c0adf0e311159bf77e2eab
canyener <can.yener@hotmail.com.tr>
2019-10-07 20:40:52 +0300

    Exports express app code except listen method to app.js for testing purposes

---
+++ src/app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+require('./db/mongoose')
+
+const userRouter = require('./routers/user')
+const taskRouter = require('./routers/task')
+
+const app = express()
+
+app.use(express.json())
+app.use('/users', userRouter)
+app.use('/tasks', taskRouter)
+
+module.exports = app
--- src/index.js
+++ src/index.js
@@ -1,15 +1,5 @@
-const express = require('express')
-require('./db/mongoose')
-
-const userRouter = require('./routers/user')
-const taskRouter = require('./routers/task')
-
-const app = express()
+const app = require('./app')
 const port = process.env.PORT
-
-app.use(express.json())
-app.use('/users', userRouter)
-app.use('/tasks', taskRouter)
 
 app.listen(port, () => {
     console.log(`Server is up on port ${port}`)
