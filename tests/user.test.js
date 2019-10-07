d729a223b85b14a528212665eb1b449f66618462
canyener <can.yener@hotmail.com.tr>
2019-10-07 21:30:17 +0300

    Adds tests for delete account endpoint

--- tests/user.test.js
+++ tests/user.test.js
@@ -67,3 +67,18 @@ test('Should NOT get profile for unauthenticated user', async () => {
             .send()
             .expect(401)
 })
+
+test('Should delete account for user', async () => {
+    await request(app)
+            .delete('/users/me')
+            .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+            .send()
+            .expect(200)
+})
+
+test('Should NOT delete account for unauthenticated user', async () => {
+    await request(app)
+            .delete('/users/me')
+            .send()
+            .expect(401)
+})
