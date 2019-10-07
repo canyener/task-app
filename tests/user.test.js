206757f1f97b0bb34dbc1a56e023b81c6cd26b83
canyener <can.yener@hotmail.com.tr>
2019-10-07 20:55:21 +0300

    Adds beforeEach function

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,6 +1,12 @@
 const request = require('supertest')
 const app = require('../src/app')
 
+const User = require('../src/models/user')
+
+beforeEach( async () => {
+    await User.deleteMany()
+})
+
 test('Should signup a new user', async () => {
     await request(app)
             .post('/users')
