2c5c1a9346c5b826e9a5bb5e7c151d796bec7d7b
canyener <can.yener@hotmail.com.tr>
2019-10-12 21:53:51 +0300

    Adds seed tasks for test cases

--- tests/fixtures/db.js
+++ tests/fixtures/db.js
@@ -2,21 +2,55 @@ const mongoose = require('mongoose')
 const jwt = require('jsonwebtoken')
 
 const User = require('../../src/models/user')
+const Task = require('../../src/models/task')
 
 const userOneId = new mongoose.Types.ObjectId()
 const userOne = {
     _id: userOneId,
-    name: 'Cem Yener',
-    email: 'cem@example.com',
-    password: 'cemcem1!',
+    name: 'User One',
+    email: 'userOne@example.com',
+    password: 'userOne1!',
     tokens: [{
         token: jwt.sign({_id: userOneId}, process.env.JWT_SECRET)
     }]
 }
 
+const userTwoId = new mongoose.Types.ObjectId()
+const userTwo = {
+    _id: userTwoId,
+    name: 'User Two',
+    email: 'userTwo@example.com',
+    password: 'userTwo1!',
+    tokens: [{
+        token: jwt.sign({_id: userTwoId}, process.env.JWT_SECRET)
+    }]
+}
+
+const taskOne = {
+    _id: new mongoose.Types.ObjectId(),
+    description: 'Test Task One Description',
+    completed: false,
+    owner: userOneId
+}
+
+const taskTwo = {
+    _id: new mongoose.Types.ObjectId(),
+    description: 'Test Task Two Description',
+    completed: true,
+    owner: userOneId
+}
+
+const taskThree = {
+    _id: new mongoose.Types.ObjectId(),
+    description: 'Test Task Two Description',
+    completed: true,
+    owner: userTwoId
+}
+
 const setupDatabase = async () => {
     await User.deleteMany()
     await new User(userOne).save()
+    await new User(userTwo).save()
 }
 
 module.exports = {
