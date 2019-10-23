747b5e6d4e9fefbba1273ed8153e36aad756e086
canyener <can.yener@hotmail.com.tr>
2019-10-23 09:23:47 +0300

    Adds seed tasks for filtering tests

--- tests/fixtures/db.js
+++ tests/fixtures/db.js
@@ -49,6 +49,41 @@ const taskThree = {
     owner: userTwoId
 }
 
+const taskFour = {
+    _id: new mongoose.Types.ObjectId(),
+    description: 'Test Task Four Description',
+    completed: false,
+    owner: userOneId
+}
+
+const taskFive = {
+    _id: new mongoose.Types.ObjectId(),
+    description: 'Test Task Five Description',
+    completed: true,
+    owner: userOneId
+}
+
+const taskSix = {
+    _id: new mongoose.Types.ObjectId(),
+    description: 'Test Task Six Description',
+    completed: false,
+    owner: userOneId
+}
+
+const taskSeven = {
+    _id: new mongoose.Types.ObjectId(),
+    description: 'Test Task Seven Description',
+    completed: false,
+    owner: userOneId
+}
+
+const taskEight = {
+    _id: new mongoose.Types.ObjectId(),
+    description: 'Test Task Eight Description',
+    completed: false,
+    owner: userOneId
+}
+
 const setupDatabase = async () => {
     await User.deleteMany()
     await Task.deleteMany()
@@ -57,6 +92,11 @@ const setupDatabase = async () => {
     await new Task(taskOne).save()
     await new Task(taskTwo).save()
     await new Task(taskThree).save()
+    await new Task(taskFour).save()
+    await new Task(taskFive).save()
+    await new Task(taskSix).save()
+    await new Task(taskSeven).save()
+    await new Task(taskEight).save()
 }
 
 module.exports = {
