e843f15d3143627f45d28202a6baa4f2fa0e2fe5
canyener <can.yener@hotmail.com.tr>
2019-10-25 19:51:31 +0300

    Sets  useUnifiedTopology to true

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 mongoose.connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useFindAndModify: false
+    useFindAndModify: false,
+    useUnifiedTopology: true
 })
 
