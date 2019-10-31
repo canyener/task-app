bde5fc6345d1257996c92aead8c7333e888624d4
canyener <can.yener@hotmail.com.tr>
2019-11-01 01:01:36 +0300

    Comments unused models

--- playground/task-user-relationship.js
+++ playground/task-user-relationship.js
@@ -1,7 +1,7 @@
 require('../src/db/mongoose')
 
 const User = require('../src/models/user')
-const Task = require('../src/models/task')
+//const Task = require('../src/models/task')
 
 const main = async () => {
     // const task = await Task.findById('5d91094ffb21ba0c74d5a7b1')
